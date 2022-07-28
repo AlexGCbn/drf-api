93483070a77054b7a262e32d1834e3d4dc2624b2
Alex Ciobanu <tsopi10@gmail.com>
2022-07-28 15:51:31 +0000

    Add profiles detail view

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ybtb=cr(f9tlz#=l$2ul3xa3i$9&coy6g0r4wq@i*5s*_ptak&'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
