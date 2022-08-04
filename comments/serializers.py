89fcd7a8b827680a4b17739d1ed85956afa5f6c8
Alex Ciobanu <tsopi10@gmail.com>
2022-08-04 14:38:22 +0000

    Add pagination and finishing touches

--- comments/serializers.py
+++ comments/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.humanize.templatetags.humanize import naturaltime
 from rest_framework import serializers
 from .models import Comment
 
@@ -7,6 +8,8 @@ class CommentSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    created_at = serializers.SerializerMethodField()
+    updated_at = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
@@ -20,6 +23,12 @@ class CommentSerializer(serializers.ModelSerializer):
             'profile_image', 'created_at',
             'updated_at', 'content'
         ]
+
+    def get_created_at(self, obj):
+        return naturaltime(obj.created_at)
+
+    def get_updated_at(self, obj):
+        return naturaltime(obj.updated_at)
 
 
 class CommentDetailSerializer(CommentSerializer):
